c150e11c336cfe2899d08a2724f79d5a7f03dbe8
Hylbert Bentes Rocha Rodrigues <hylbert.rocha@VENTTOS.local>
2023-05-03 09:25:17 -0400

    First widget

--- learnGUI.py
+++ learnGUI.py
@@ -1,9 +1,22 @@
+'''
+Fonte: https://www.devmedia.com.br/tkinter-interfaces-graficas-em-python/33956
+
+'''
 from tkinter import*
 
 class Application:
     def __init__(self, master = None):
-        pass
+        self.widget1 = Frame(master) 
+        ''' O frame master é o nosso top level, que é o elemento máximo da hierarquia, 
+        ou seja, é a nossa janela de aplicação, que contém o título, e botões de maximizar, minimizar e fechar.'''
+        self.widget1.pack()
+        self.msg = Label(self.widget1, text = 'Primeiro widget')
+        self.msg.pack()
 
-root = Tk()
+root = Tk() # Essa classe permite que os widgets possam ser utilizados na aplicação.
 Application(root)
+'''
+Em Application(root) passamos a variável root como parâmetro do método construtor da classe Application.
+E para finalizar, chamamos o método root.mainloop() para exibirmos a tela. Sem o event loop, a interface não será exibida.
+'''
 root.mainloop()
