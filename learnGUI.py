cd378c6fb8d338f759ba3a648a30b8983a144226
Hylbert <hylbertb01@gmail.com>
2023-05-03 22:41:21 -0400

    Receiving user data

--- learnGUI.py
+++ learnGUI.py
@@ -2,30 +2,70 @@
 Fonte: https://www.devmedia.com.br/tkinter-interfaces-graficas-em-python/33956
 
 '''
+from operator import le
 from tkinter import*
+from tkinter import font
+from turtle import left
 
 class Application:
     def __init__(self, master = None):
-        self.widget1 = Frame(master) 
-        ''' O frame master é o nosso top level, que é o elemento máximo da hierarquia, 
-        ou seja, é a nossa janela de aplicação, que contém o título, e botões de maximizar, minimizar e fechar.'''
-        self.widget1.pack()
-        self.msg = Label(self.widget1, text = 'Primeiro widget')
-        self.msg['font'] = ('Calibri', '9', 'italic')
-        self.msg.pack()
-        self.sair = Button(self.widget1)
-        self.sair['text'] = 'Clique aqui'
-        self.sair['font'] = ('Calibri', '9')
-        self.sair['width'] = 10
-        self.sair.bind('<Button-1>', self.mudarTexto) # event handler = são ações executadas como resposta a determinado evento
-        #ou self.sair['command'] = self.mudarTexto
-        self.sair.pack() # SIDE =  Define em que lado o widget se posicionará (Left, Right, Top, Bottom)
+        self.fontePadrao = ('Arial', '10')
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer['pady'] = 10
+        self.primeiroContainer.pack()
 
-    def mudarTexto(self, event): # event handler
-        if self.msg['text'] == 'Primeiro widget':
-            self.msg['text'] = 'O botão recebeu um clique'
+        self.segundoContainer = Frame(master)
+        self.segundoContainer['padx'] = 20
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer['padx'] = 20
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer['pady'] = 20
+        self.quartoContainer.pack()
+
+        self.titulo = Label(self.primeiroContainer, text='Dados do usuário')
+        self.titulo['font'] = ('Arial', '10', 'bold')
+        self.titulo.pack()
+
+        self.nomeLabel = Label(self.segundoContainer, text='Nome', font=self.fontePadrao)
+        self.nomeLabel.pack(side=LEFT)
+
+        self.nome = Entry(self.segundoContainer)
+        self.nome['width'] = 30
+        self.nome['font'] = self.fontePadrao
+        self.nome.pack(side=LEFT)
+
+        self.senhaLabel = Label(self.terceiroContainer, text='Senha', font = self.fontePadrao)
+        self.senhaLabel.pack(side=LEFT)
+
+        self.senha = Entry(self.terceiroContainer)
+        self.senha['width'] = 30
+        self.senha['font'] = self.fontePadrao
+        self.senha['show'] = '*'
+        self.senha.pack(side=LEFT)
+
+        self.autenticar = Button(self.quartoContainer)
+        self.autenticar['text'] = 'Autenticar'
+        self.autenticar['font'] = ('Calibri', '8')
+        self.autenticar['width'] = 12
+        self.autenticar['command'] = self.verificaSenha
+        self.autenticar.pack()
+
+        self.mensagem = Label(self.quartoContainer, text='', font=self.fontePadrao)
+        self.mensagem.pack()
+
+    #Método verifica senha
+    def verificaSenha(self):
+        usuario = self.nome.get()
+        senha = self.senha.get()
+        if usuario == 'hylbert' and senha == 'dev':
+            self.mensagem['text'] = 'Autenticado'
         else:
-            self.msg['text'] = 'Primeiro widget'
+            self.mensagem['text'] = 'Erro na autenticação'
+     
 root = Tk() # Essa classe permite que os widgets possam ser utilizados na aplicação.
 Application(root)
 '''
