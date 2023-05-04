7759fde7ba15af99d37e1b0ff7da95ace43e835c
Hylbert <hylbertb01@gmail.com>
2023-05-03 22:14:05 -0400

    update 'mudarTexto'

--- learnGUI.py
+++ learnGUI.py
@@ -11,14 +11,21 @@ class Application:
         ou seja, é a nossa janela de aplicação, que contém o título, e botões de maximizar, minimizar e fechar.'''
         self.widget1.pack()
         self.msg = Label(self.widget1, text = 'Primeiro widget')
-        self.msg['font'] = ('Verdana', '10', 'italic', 'bold')
+        self.msg['font'] = ('Calibri', '9', 'italic')
         self.msg.pack()
         self.sair = Button(self.widget1)
-        self.sair['text'] = 'Sair'
-        self.sair['font'] = ('Verdana', '10')
-        self.sair['width'] = 5
-        self.sair['command'] = self.widget1.quit
-        self.sair.pack(side=RIGHT) # SIDE =  Define em que lado o widget se posicionará (Left, Right, Top, Bottom)
+        self.sair['text'] = 'Clique aqui'
+        self.sair['font'] = ('Calibri', '9')
+        self.sair['width'] = 10
+        self.sair.bind('<Button-1>', self.mudarTexto) # event handler = são ações executadas como resposta a determinado evento
+        #ou self.sair['command'] = self.mudarTexto
+        self.sair.pack() # SIDE =  Define em que lado o widget se posicionará (Left, Right, Top, Bottom)
+
+    def mudarTexto(self, event): # event handler
+        if self.msg['text'] == 'Primeiro widget':
+            self.msg['text'] = 'O botão recebeu um clique'
+        else:
+            self.msg['text'] = 'Primeiro widget'
 root = Tk() # Essa classe permite que os widgets possam ser utilizados na aplicação.
 Application(root)
 '''
