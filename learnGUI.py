278dcac9f9738e9831d6b0e9e12ff8d6032584ba
Hylbert <hylbertb01@gmail.com>
2023-05-03 21:57:50 -0400

    Addition of SIDE configuration

--- learnGUI.py
+++ learnGUI.py
@@ -15,11 +15,10 @@ class Application:
         self.msg.pack()
         self.sair = Button(self.widget1)
         self.sair['text'] = 'Sair'
-        self.sair['font'] = ('Calibri', '10')
+        self.sair['font'] = ('Verdana', '10')
         self.sair['width'] = 5
         self.sair['command'] = self.widget1.quit
-        self.sair.pack()
-
+        self.sair.pack(side=RIGHT) # SIDE =  Define em que lado o widget se posicionará (Left, Right, Top, Bottom)
 root = Tk() # Essa classe permite que os widgets possam ser utilizados na aplicação.
 Application(root)
 '''
